b0831983bc19b36aaf6b27d90d6e7508d889addb
nguyenhai9112 <nguyenhai9112@gmail.com>
2018-12-30 21:23:44 +0700

    update fragments header

--- src/main/java/com/project/shopcart/controller/CartController.java
+++ src/main/java/com/project/shopcart/controller/CartController.java
@@ -4,8 +4,6 @@ import com.project.shopcart.model.Item;
 import com.project.shopcart.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
